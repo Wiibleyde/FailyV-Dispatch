c26a49317e2abcb792e6ab8b5a477317926d8455
Wiibleyde <nathan@bonnell.fr>
2023-07-30 18:59:37 +0200

    Add accent to names
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- services/regexFunc.py
+++ services/regexFunc.py
@@ -4,7 +4,7 @@ from services.ObjectsService import DocteurObj, AgentObj
 class RegexUtils:
     def doctorToList(string, grade):
         docs = []
-        regex = r"\d{2}/\d{2}/\d{4}\s-\s\d{3}-\d{4}\s(?P<first_name>[A-Z][a-z]*)\s(?P<last_name>(?:[A-Z][a-z]*\s*)+$)"
+        regex = r"\d{2}/\d{2}/\d{4}\s-\s\d{3}-\d{4}\s(?P<first_name>[A-Z][a-zà-ü]*)\s(?P<last_name>(?:[A-Z][a-zà-ü]*\s*)+$)"
         matches = re.finditer(regex, string, re.MULTILINE)
         for _, match in enumerate(matches, start=1):
             doc = DocteurObj(0, match.group('last_name'), match.group('first_name'), grade, False, False, False, False)
@@ -13,7 +13,7 @@ class RegexUtils:
     
     def agentToList(string, grade):
         agents = []
-        regex = r"-(?P<number>\s\d+)\s:\s(?P<first_name>[A-Z][a-zA-Z\-\ ]*)\s(?P<last_name>[A-Z][a-zA-Z\-\ ]*)\s\(\d+-\d+\)"
+        regex = r"-(?P<number>\s\d+)\s:\s(?P<first_name>[A-Z][a-zà-üA-ZÀ-Ü\-\ ]*)\s(?P<last_name>[A-Z][a-zà-üA-ZÀ-Ü\-\ ]*)\s\(\d+-\d+\)"
         matches = re.finditer(regex, string, re.MULTILINE)
         for _, match in enumerate(matches, start=1):
             agent = AgentObj(0, match.group('number'), match.group('last_name'), match.group('first_name'), grade, False, False, False, False)
@@ -21,13 +21,13 @@ class RegexUtils:
         return agents
     
     def doctorToString(string, grade):
-        regex = r"(?P<first_name>[A-Z][a-z]*)\s(?P<last_name>(?:[A-Z][a-z]*\s*)+$)"
+        regex = r"(?P<first_name>[A-Z][a-z]*)\s(?P<last_name>(?:[A-Z][a-zà-ü]*\s*)+$)"
         matches = re.finditer(regex, string, re.MULTILINE)
         for _, match in enumerate(matches, start=1):
             return DocteurObj(0, match.group('last_name'), match.group('first_name'), grade, False, False, False, False)
         
     def agentToString(string, matricule, grade):
-        regex = r"(?P<first_name>[A-Z][a-z]*)\s(?P<last_name>(?:[A-Z][a-z]*\s*)+$)"
+        regex = r"(?P<first_name>[A-Z][a-z]*)\s(?P<last_name>(?:[A-Z][a-zà-ü]*\s*)+$)"
         matches = re.finditer(regex, string, re.MULTILINE)
         for _, match in enumerate(matches, start=1):
             return AgentObj(0, matricule, match.group('last_name'), match.group('first_name'), grade, False, False, False, False)
